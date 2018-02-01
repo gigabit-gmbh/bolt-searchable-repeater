94c9c5df1b3ddc6a8a7fc9055ce4eea80c768837
Thomas Helmrich <thomas@gigabit.de>
2018-02-01 16:20:51 +0100

    created helper services to use own legacy storage

--- src/SearchAbleRepeaterExtension.php
+++ src/SearchAbleRepeaterExtension.php
@@ -2,7 +2,9 @@
 
 namespace Bolt\Extension\Gigabit\SearchableRepeater;
 
+use Bolt\Extension\Gigabit\SearchableRepeater\Helpers\Storage;
 use Bolt\Extension\SimpleExtension;
+use Bolt\Storage\EntityManager;
 use Silex\Application;
 
 /**
@@ -18,11 +20,45 @@ class SearchAbleRepeaterExtension extends SimpleExtension
      */
     protected function registerServices(Application $app)
     {
+        $app['helper.storage.legacy'] = $app->share(
+            function ($app) {
+                return new Storage($app);
+            }
+        );
+
+        $app['helper.storage'] = $app->share(
+            function ($app) {
+                $storage = new EntityManager(
+                    $app['db'],
+                    $app['dispatcher'],
+                    $app['storage.metadata'],
+                    $app['logger.system']
+                );
+                $storage->setLegacyService($app['storage.legacy_service']);
+                $storage->setLegacyStorage($app['helper.storage.legacy']);
+                $storage->setEntityBuilder($app['storage.entity_builder']);
+                $storage->setFieldManager($app['storage.field_manager']);
+                $storage->setCollectionManager($app['storage.collection_manager']);
+
+                foreach ($app['storage.repositories'] as $entity => $repo) {
+                    $storage->setRepository($entity, $repo);
+                }
+
+                $storage->setDefaultRepositoryFactory($app['storage.content_repository']);
+
+                return $storage;
+            }
+        );
+
+
         $app['controller.repeater-search'] = $app->share(
             function ($app) {
                 return new Controller\SearchController($app);
             }
         );
+
+
+
     }
 
 }
